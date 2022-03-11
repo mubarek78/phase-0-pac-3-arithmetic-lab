ffeaa79a855b5752bc4403702b11bc951043aa8e
mubarek78 <shifamubarek@yahoo.com>
2022-03-10 21:48:52 -0700

    Completed assignment

--- test/index-test.js
+++ test/index-test.js
@@ -10,43 +10,85 @@ describe('basic math functions', function () {
     expect(add).toExist
   })
 
+let number = 10;
+
+function add() {
+  return (number += 5);
+}
+
+
   it("'subtract()' is a valid function", function() {
     expect(subtract).toExist
   })
 
+  function subtract() {
+    return (number += 5);
+  }
+  
   it("'multiply()' is a valid function", function() {
     expect(multiply).toExist
   })
+
+  function multiply() {
+    return (number += 5);
+  }
 
   it("'divide()' is a valid function", function() {
     expect(divide).toExist
   })
 
+  function divide() {
+    return (number += 5);
+  }
+
   it('add(a, b) adds two numbers and returns the result', function() {
     expect(add(a, b)).toEqual(a + b)
   })
   
+  function add(a, b) {
+    return (a + b);
+  }
+
   it('subtract(a, b) subtracts b from a and returns the result', function() {
     expect(subtract(a, b)).toEqual(a - b)
   })
   
+  function subtract(a, b) {
+    return (a - b);
+  }
+
   it('multiply(a, b) multiplies two numbers and returns the result', function() {
     expect(multiply(a, b)).toEqual(a * b)
   })
   
+  function multiply(a, b) {
+    return (a * b);
+  }
+
   it('divide(a, b) divides a by b and returns the result', function() {
     expect(divide(a, b)).toEqual(a / b)
   })
   
+  function divide(a, b) {
+    return (a / b);
+  }
+
   it('increment(n) increments n and returns the result', function() {
     expect(increment(a)).toEqual(a + 1)
   })
   
+  function increment(a) {
+    return (a + 1);
+  }
+
   it('decrement(n) decrements n and returns the result', function() {
     expect(decrement(a)).toEqual(a - 1)
   })
 })
 
+function decrement(a) {
+  return (a - 1);
+}
 
 
 
@@ -55,19 +97,33 @@ describe('makeInt(n)', function() {
     expect(makeInt(a.toString())).toEqual(a)
   })
 
+  function makeInt(a) {
+    return (parseInt('a', 10));
+  }
+
   it('assumes base 10', function() {
     expect(makeInt('0x2328')).toEqual(0)
   })
+
+  function makeInt(a){
+   return parseInt(a, 10)
+    }
 
   it('returns NaN as appropriate', function() {
     expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
   })
 })
 
+
+
 describe('preserveDecimal(n)', function() {
   it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
     expect(preserveDecimal('2.222')).toBe(2.222)
   })
+
+ function preserveDecimal(n){
+    return parseFloat(n)
+  }
 
   it('returns NaN as appropriate', function() {
     expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
